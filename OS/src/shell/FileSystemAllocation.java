f9447c921468e37146c963906911a050e06afba7
niksa-k <niksakascelan02@gmail.com>
2024-05-29 15:42:05 +0200

    Dodavanje vektora bitova za prazan prostor sistema datoteka

--- OS/src/shell/FileSystemAllocation.java
+++ OS/src/shell/FileSystemAllocation.java
@@ -1,70 +1,65 @@
 package shell;
+
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
+
 public class FileSystemAllocation {
+    // Bitset koji predstavlja prazan prostor
+    private static BitSet emptySpace;
+    // Veličina bloka u megabajtima
+    private static final int blockSizeInMB = 4;
+    // Ukupan broj blokova u memoriji diska
+    private static final int TOTAL_DISC_MEMORY_BLOCKS = 256;
 
-        // Lista blokova u memoriji diska
-        static List<Block> discMemoryBlocks = new ArrayList<>();
-        // Ukupan broj blokova u memoriji diska
-        private static final int TOTAL_DISC_MEMORY_BLOCKS = 256;
-        // Veličina bloka u megabajtima
-        static final int blockSizeInMB = 4;
-
-        // Metoda za alokaciju blokova memorije
-        public List<Block> allocateMemoryBlocks(int fileSizeInMB) {
-            // Lista alokovanih blokova
-            List<Block> allocatedBlocks = new ArrayList<>();
-            // Broj blokova potreban za datu veličinu fajla
-            int blocksRequired = (int) Math.ceil((double) fileSizeInMB / blockSizeInMB);
-
-            // Brojač za kontinuirane blokove
-            int contiguousCount = 0;
-
-            // Iteracija kroz blokove memorije
-            for (Block block : discMemoryBlocks) {
-                if (!block.isAllocated()) {
-                    contiguousCount++;
-                    allocatedBlocks.add(block);
-
-                    // Ako je pronađen dovoljan broj kontinuiranih blokova
-                    if (contiguousCount == blocksRequired) {
-                        // Alokacija blokova
-                        for (Block allocatedBlock : allocatedBlocks) {
-                            allocatedBlock.allocate();
-                        }
-                        // Vraćanje alokovanih blokova
-                        return allocatedBlocks;
-                    }
-                } else {
-                    // Resetovanje brojača i liste ako se prekine niz slobodnih blokova
-                    contiguousCount = 0;
-                    allocatedBlocks.clear();
-                }
-            }
-
-            // Dealokacija blokova ako nije moguće naći dovoljan broj kontinuiranih blokova
-            for (Block block : allocatedBlocks) {
-                block.deallocate();
-            }
-
-            return null;
-        }
-
-        // Metoda za dealokaciju blokova memorije
-        public void deallocateMemoryBlocks(List<Block> blocksToDeallocate) {
-            for (Block block : blocksToDeallocate) {
-                block.deallocate();
-            }
-        }
-
-        // Metoda za kreiranje blokova memorije
-        public static void createMemoryBlocks() {
-            int startAddress = 0; // Početna adresa za prvi blok
-            for (int i = 0; i < TOTAL_DISC_MEMORY_BLOCKS; i++) {
-                discMemoryBlocks.add(new Block(startAddress, blockSizeInMB));
-                startAddress += blockSizeInMB; // Povećaj početnu adresu za veličinu bloka
-            }
-        }
+    // Inicijalizacija praznog prostora
+    public static void initializeEmptySpace() {
+        emptySpace = new BitSet(TOTAL_DISC_MEMORY_BLOCKS);
+        emptySpace.set(0, TOTAL_DISC_MEMORY_BLOCKS); // Postavljanje svih blokova kao slobodne
     }
 
+    // Metoda za alokaciju blokova memorije
+    public static List<Block> allocateMemoryBlocks(int fileSizeInMB) {
+        List<Block> allocatedBlocks = new ArrayList<>();
+        int blocksRequired = (int) Math.ceil((double) fileSizeInMB / blockSizeInMB);
 
+        int startIndex = findContiguousBlocks(blocksRequired);
+        if (startIndex != -1) {
+            for (int i = startIndex; i < startIndex + blocksRequired; i++) {
+                Block block = new Block(i * blockSizeInMB, blockSizeInMB);
+                block.allocate();
+                allocatedBlocks.add(block);
+                emptySpace.clear(i);
+            }
+            return allocatedBlocks;
+        }
+
+        return null;
+    }
+
+    // Pronalaženje kontinuiranih blokova u praznom prostoru
+    private static int findContiguousBlocks(int blocksRequired) {
+        int startIndex = emptySpace.nextClearBit(0);
+        int contiguousCount = 0;
+        for (int i = startIndex; i < TOTAL_DISC_MEMORY_BLOCKS; i++) {
+            if (!emptySpace.get(i)) {
+                contiguousCount++;
+                if (contiguousCount == blocksRequired) {
+                    return i - blocksRequired + 1;
+                }
+            } else {
+                contiguousCount = 0;
+            }
+        }
+        return -1;
+    }
+
+    // Metoda za dealokaciju blokova memorije
+    public static void deallocateMemoryBlocks(List<Block> blocksToDeallocate) {
+        for (Block block : blocksToDeallocate) {
+            int blockIndex = block.getStartAddress() / blockSizeInMB;
+            emptySpace.set(blockIndex);
+            block.deallocate();
+        }
+    }
+}
--- OS/src/shell/FileSystemOrganization.java
+++ OS/src/shell/FileSystemOrganization.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.List;
 public class FileSystemOrganization {
 
-
     // Ime direktorijuma
     private String name;
     // Lista poddirektorijuma
@@ -21,123 +20,122 @@ public class FileSystemOrganization {
         parent = null;
     }
 
-        // Dodavanje poddirektorijuma
-        public void addDirectory(FileSystemOrganization directory) {
+    // Dodavanje poddirektorijuma
+    public void addDirectory(FileSystemOrganization directory) {
             subdirectories.add(directory);
         }
 
-        // Dodavanje fajla
-        public void addFile(File file) {
-            files.add(file);
-        }
+    // Dodavanje fajla
+    public void addFile(File file) {
+        files.add(file);
+    }
 
-        // Getter za ime direktorijuma
-        public String getName() {
+    // Getter za ime direktorijuma
+    public String getName() {
             return name;
         }
 
-        // Setter za ime direktorijuma
-        public void setName(String name) {
+    // Setter za ime direktorijuma
+    public void setName(String name) {
             this.name = name;
         }
 
-        // Getter za listu poddirektorijuma
-        public List<FileSystemOrganization> getSubdirectories() {
+    // Getter za listu poddirektorijuma
+    public List<FileSystemOrganization> getSubdirectories() {
             return subdirectories;
         }
 
-        // Setter za listu poddirektorijuma
-        public void setSubdirectories(List<FileSystemOrganization> subdirectories) {
-            this.subdirectories = subdirectories;
-        }
+    // Setter za listu poddirektorijuma
+    public void setSubdirectories(List<FileSystemOrganization> subdirectories) {
+        this.subdirectories = subdirectories;
+    }
 
-        // Getter za listu fajlova
-        public List<File> getFiles() {
+    // Getter za listu fajlova
+    public List<File> getFiles() {
             return files;
         }
 
-        // Setter za listu fajlova
-        public void setFiles(List<File> files) {
+    // Setter za listu fajlova
+    public void setFiles(List<File> files) {
             this.files = files;
         }
 
-        // Metoda za prikaz imena direktorijuma
-        @Override
-        public String toString() {
+    // Metoda za prikaz imena direktorijuma
+    @Override
+    public String toString() {
             return name;
         }
 
-        // Getter za roditeljski direktorijum
-        public FileSystemOrganization getParent() {
+    // Getter za roditeljski direktorijum
+    public FileSystemOrganization getParent() {
             return parent;
         }
 
-        // Setter za roditeljski direktorijum
-        public void setParent(FileSystemOrganization parent) {
+    // Setter za roditeljski direktorijum
+    public void setParent(FileSystemOrganization parent) {
             this.parent = parent;
         }
 
-        // Metoda za kreiranje novog poddirektorijuma
-        public FileSystemOrganization createDirectory(String name) {
-            FileSystemOrganization newDirectory = new FileSystemOrganization(name);
-            subdirectories.add(newDirectory);
-            newDirectory.setParent(this);
-            return newDirectory;
-        }
+    // Metoda za kreiranje novog poddirektorijuma
+    public FileSystemOrganization createDirectory(String name) {
+        FileSystemOrganization newDirectory = new FileSystemOrganization(name);
+        subdirectories.add(newDirectory);
+        newDirectory.setParent(this);
+        return newDirectory;
+    }
 
-        // Metoda za kreiranje novog fajla
-        public void createFile(String name, int sizeInMB, List<Block> allocatedBlocks) {
-            File newFile = new File(name, sizeInMB);
-            newFile.setAllocatedBlocks(allocatedBlocks); // Postavljanje alokovanih blokova za fajl
-            files.add(newFile);
-        }
+    // Metoda za kreiranje novog fajla
+    public void createFile(String name, int sizeInMB, List<Block> allocatedBlocks) {
+        File newFile = new File(name, sizeInMB);
+        newFile.setAllocatedBlocks(allocatedBlocks); // Postavljanje alokovanih blokova za fajl
+        files.add(newFile);
+    }
 
-        // Metoda za pronalaženje poddirektorijuma po imenu
-        public FileSystemOrganization getSubdirectoryByName(String name) {
-            for (FileSystemOrganization subdir : subdirectories) {
-                if (subdir.getName().equals(name)) {
-                    return subdir;
-                }
-            }
-            return null;
-        }
-
-        // Metoda za promenu trenutnog direktorijuma na poddirektorijum
-        public FileSystemOrganization changeToSubdirectory(String name) {
-            FileSystemOrganization subdir = getSubdirectoryByName(name);
-            if (subdir != null) {
+    // Metoda za pronalaženje poddirektorijuma po imenu
+    public FileSystemOrganization getSubdirectoryByName(String name) {
+        for (FileSystemOrganization subdir : subdirectories) {
+            if (subdir.getName().equals(name)) {
                 return subdir;
-            } else {
-                System.out.println("Directory not found: " + name);
-                return this;
             }
         }
+        return null;
+    }
 
-        // Metoda za brisanje direktorijuma
-        public void deleteDirectory(String name) {
-            FileSystemOrganization directoryToDelete = getSubdirectoryByName(name);
-            if (directoryToDelete != null) {
-                subdirectories.remove(directoryToDelete);
-            }
-        }
-
-        // Metoda za brisanje fajla
-        public void deleteFile(String name) {
-            File fileToDelete = getFileByName(name);
-            if (fileToDelete != null) {
-                files.remove(fileToDelete);
-            }
-        }
-
-        // Metoda za pronalaženje fajla po imenu
-        private File getFileByName(String name) {
-            for (File file : files) {
-                if (name.equals(file.getName())) {
-                    return file;
-                }
-            }
-            return null;
+    // Metoda za promenu trenutnog direktorijuma na poddirektorijum
+    public FileSystemOrganization changeToSubdirectory(String name) {
+        FileSystemOrganization subdir = getSubdirectoryByName(name);
+        if (subdir != null) {
+            return subdir;
+        } else {
+            System.out.println("Directory not found: " + name);
+            return this;
         }
     }
+
+    // Metoda za brisanje direktorijuma
+    public void deleteDirectory(String name) {
+        FileSystemOrganization directoryToDelete = getSubdirectoryByName(name);
+        if (directoryToDelete != null) {
+            subdirectories.remove(directoryToDelete);
+        }
+    }
+    // Metoda za brisanje fajla
+    public void deleteFile(String name) {
+        File fileToDelete = getFileByName(name);
+        if (fileToDelete != null) {
+            files.remove(fileToDelete);
+        }
+    }
+
+    // Metoda za pronalaženje fajla po imenu
+    private File getFileByName(String name) {
+        for (File file : files) {
+            if (name.equals(file.getName())) {
+                return file;
+            }
+        }
+        return null;
+    }
+}
 
 
